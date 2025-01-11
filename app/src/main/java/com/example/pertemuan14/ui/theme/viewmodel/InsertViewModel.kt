757da97549b231d9370af40fc43305d33690f7da
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2025-01-12 04:41:42 +0700

    membuat data class form error

--- app/src/main/java/com/example/pertemuan14/ui/theme/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/theme/viewmodel/InsertViewModel.kt
@@ -72,6 +72,21 @@ data class InsertUiState(
     val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isEntryValid: FormErrorState = FormErrorState(),
 )
+// data class Form Error State
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenis_kelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenis_kelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
 
 
 
