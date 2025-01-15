c748628db9156ae13df1ac3c28b7d4479c23f204
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2025-01-15 10:54:48 +0700

    Memperbaiki container

--- app/src/main/java/com/example/pertemuan14/di/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pertemuan14/di/MahasiswaContainer.kt
@@ -1,16 +1,18 @@
 package com.example.pertemuan14.di
 
+import android.content.Context
 import com.example.pertemuan14.repository.MahasiswaRepository
-import com.example.pertemuan14.repository.NetworkMahasiswaRepository
+import com.example.pertemuan14.repository.NetworkRepositoryMhs
 import com.google.firebase.firestore.FirebaseFirestore
 
-interface AppContainer {
-    val mahasiswaRepository : MahasiswaRepository
-}
-class MahasiswaContainer : AppContainer {
-   private val firebase: FirebaseFirestore = FirebaseFirestore.getInstance()
 
+interface AppContainer {
+    val mahasiswaRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer(private val context: Context) : InterfaceContainerApp {
+    private val firestore : FirebaseFirestore = FirebaseFirestore.getInstance()
     override val mahasiswaRepository: MahasiswaRepository by lazy {
-        NetworkMahasiswaRepository(firebase)
+        NetworkRepositoryMhs(firestore)
     }
 }
