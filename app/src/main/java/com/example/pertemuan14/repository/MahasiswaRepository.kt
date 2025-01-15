ba4f11bcc4d01c8b7bc4132739627661dffb7de2
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2025-01-15 10:52:30 +0700

    Mengganti update menjadi dari nim menjadi mahasiswa

--- app/src/main/java/com/example/pertemuan14/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/pertemuan14/repository/MahasiswaRepository.kt
@@ -4,9 +4,9 @@ import com.example.pertemuan14.model.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
 interface MahasiswaRepository {
-    suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
+    fun getMahasiswa() : Flow<List<Mahasiswa>>
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
-    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
     suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
-    suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa>
+    fun getMahasiswaByNim(nim: String) : Flow<Mahasiswa>
 }
