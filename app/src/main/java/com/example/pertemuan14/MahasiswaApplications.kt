7502310508d2f1aa00704b6e00f183134d75d574
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2025-01-15 10:51:29 +0700

    Menambah initialize app untuk firebase

--- app/src/main/java/com/example/pertemuan14/MahasiswaApplications.kt
+++ app/src/main/java/com/example/pertemuan14/MahasiswaApplications.kt
@@ -1,14 +1,15 @@
 package com.example.pertemuan14
 
 import android.app.Application
-import com.example.pertemuan14.di.AppContainer
 import com.example.pertemuan14.di.MahasiswaContainer
+import com.google.firebase.FirebaseApp
 
 class MahasiswaApplications : Application() {
     lateinit var container: MahasiswaContainer
 
     override fun onCreate() {
         super.onCreate()
-        container = MahasiswaContainer()
+        FirebaseApp.initializeApp(this)
+        container = MahasiswaContainer(this)
     }
 }
