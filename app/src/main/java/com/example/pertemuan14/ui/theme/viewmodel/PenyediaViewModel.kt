d08625ffd29e18d54beeaf355bafdac79d81736d
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2025-01-15 10:53:26 +0700

    memperbaiki PenyediaViewModel

--- app/src/main/java/com/example/pertemuan14/ui/theme/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/theme/viewmodel/PenyediaViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.pertemuan14.ui.theme.viewmodel
 
+
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
@@ -7,15 +8,15 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pertemuan14.MahasiswaApplications
 
 
-object PenyediaViewModel{
+object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
-            HomeViewModel(
-                MahasiswaApplications().container.mahasiswaRepository)
+            val app = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications
+            HomeViewModel(app.container.mahasiswaRepository)
         }
         initializer {
-            InsertViewModel(
-                MahasiswaApplications().container.mahasiswaRepository)
+            val app = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications
+            InsertViewModel(app.container.mahasiswaRepository)
         }
     }
 }
